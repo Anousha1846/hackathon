d14cbf8f13b9c15579722f7d759c2244f245a416
Anousha1846 <125111686+Anousha1846@users.noreply.github.com>
2023-11-30 18:15:09 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+// contact start
+
+function hoGya(){
+
+    var name = document.getElementById('name')
+    var Email = document.getElementById('Email')
+    var CONCERN = document.getElementById('CONCERN')
+    var NIC = document.getElementById('NIC')
+
+
+if(name.value === ""){
+    alert("Fill Name")
+}
+ else if(Email.value === ""){
+    alert('Fill Email')
+}else if(CONCERN.value === ""){
+    alert('Fill Concern')
+}else if(NIC.value === ""){
+    alert('Fill NIC')
+}
+else{
+    alert('THE FORM IS SUBMITTED SUCCESSFULLY')
+}
+}
+// end
+
+
+
+
+   
+
+
+
+
+
+
+
+
+
+
